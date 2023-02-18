cbb13f3e8bb167c0f4cfcab4a62b1c0085b1162f
Omar Elamri <ea_do@icloud.com>
2023-02-18 00:10:50 -0800

    Add OpenAI dep

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "APIConnection",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,9 +24,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "APIConnection",
-            dependencies: []),
+            dependencies: [.product(name: "OpenAIKit", package: "openai-kit")]),
         .testTarget(
             name: "APIConnectionTests",
             dependencies: ["APIConnection"]),
+        
     ]
 )
--- Sources/APIConnection/APIConnection.swift
+++ Sources/APIConnection/APIConnection.swift
@@ -1,6 +1,17 @@
+import OpenAIKit
+import AsyncHTTPClient
+
+
 public struct APIConnection {
     public private(set) var text = "Hello, World!"
+    
+    private static var apiKey = ""
+    private static var organization = ""
 
     public init() {
+        let httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
+        let configuration = Configuration(apiKey: APIConnection.apiKey, organization: APIConnection.organization)
+
+        let openAIClient = OpenAIKit.Client(httpClient: httpClient, configuration: configuration)
     }
 }
