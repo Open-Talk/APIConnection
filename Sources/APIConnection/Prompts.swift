6d3a3bd4946da51b95af4e09b335f99621dbadbd
Arya Gharib <40894911+Aryakhan81@users.noreply.github.com>
2023-02-18 21:08:34 -0800

    Add new cases to Views enum

--- Sources/APIConnection/Prompts.swift
+++ Sources/APIConnection/Prompts.swift
@@ -9,6 +9,9 @@
 public enum Views: String {
     case Interview
     case OrderCoffee
+    case Conversation
+    case Skins
+    case Voices
 }
 
 func promptFor(view: Views, history: [(String, String)]) -> String {
@@ -23,6 +26,10 @@ I will give you a transcript of a job interview. Specifically, of a prospective 
 \nReply with only the Interviewer's response, which should be 50 words. Do not add any quotation marks to your response.
 """
     case .OrderCoffee:
+        return ""
+    case .Conversation:
+        return "" //Keep this, free conversation
+    default: //For non-chat screens
         return ""
     }
 }
